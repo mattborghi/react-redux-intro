07f52d92e8f8201c0a347d265f304450b4d8ae0f
Matias Borghi <borghi.matias@gmail.com>
2022-02-23 18:03:32 -0300

    add component for observable part

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import { Counter as ObservableCounter } from "./features/observable-counter/Counter";
+import "./App.css";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
+        <ObservableCounter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
